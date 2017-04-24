53b4fb36a9ce7446f05811fe75cbf3121559f185
Jenny Duckett <jenny.duckett@digital.cabinet-office.gov.uk>
2017-04-24 16:09:02 +0100

    Convert self-manage-iam-user policy into policy document

--- terraform/policies.tf
+++ terraform/policies.tf
@@ -18,72 +18,81 @@ resource "aws_iam_policy" "assume-any-role" {
     policy = "${data.aws_iam_policy_document.assume_any_role.json}"
 }
 
+data "aws_iam_policy_document" "self_manage_iam_user" {
+    statement {
+        effect = "Allow"
+        actions = [
+            "iam:*LoginProfile",
+            "iam:*AccessKey*",
+            "iam:*SSHPublicKey*"
+        ]
+        resources = [
+            "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/$${aws:username}",
+        ]
+    }
+
+    statement {
+        effect = "Allow"
+        actions = [
+            "iam:ListAccount*",
+            "iam:GetAccountSummary",
+            "iam:GetAccountPasswordPolicy",
+            "iam:ListUsers"
+        ]
+        resources = [
+            "*",
+        ]
+    }
+
+    statement {
+        sid = "AllowUsersToCreateEnableResyncDeleteTheirOwnVirtualMFADevice"
+        effect = "Allow"
+        actions = [
+            "iam:CreateVirtualMFADevice",
+            "iam:EnableMFADevice",
+            "iam:ResyncMFADevice",
+            "iam:DeleteVirtualMFADevice"
+        ]
+        resources = [
+            "arn:aws:iam::${data.aws_caller_identity.current.account_id}:mfa/$${aws:username}",
+            "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/$${aws:username}"
+        ]
+    }
+
+    statement = {
+        sid = "AllowUsersToDeactivateTheirOwnVirtualMFADevice"
+        effect = "Allow"
+        actions = [
+            "iam:DeactivateMFADevice"
+        ]
+        resources = [
+            "arn:aws:iam::${data.aws_caller_identity.current.account_id}:mfa/$${aws:username}",
+            "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/$${aws:username}"
+        ]
+        condition {
+            test = "Bool"
+            variable = "aws:MultiFactorAuthPresent"
+            values = [
+                "true",
+            ]
+        }
+    }
+
+    statement {
+        sid = "AllowUsersToListMFADevicesandUsersForConsole"
+        effect = "Allow"
+        actions = [
+            "iam:ListMFADevices",
+            "iam:ListVirtualMFADevices"
+        ]
+        resources = [
+            "*",
+        ]
+    }
+}
+
 resource "aws_iam_policy" "self-manage-iam-user" {
     name = "self-manage-iam-user"
     description = "Allows the user to manage their own credentials and list all users"
-    policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "iam:*LoginProfile",
-                "iam:*AccessKey*",
-                "iam:*SSHPublicKey*"
-            ],
-            "Resource": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/$${aws:username}"
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "iam:ListAccount*",
-                "iam:GetAccountSummary",
-                "iam:GetAccountPasswordPolicy",
-                "iam:ListUsers"
-            ],
-            "Resource": "*"
-        },
-        {
-            "Sid": "AllowUsersToCreateEnableResyncDeleteTheirOwnVirtualMFADevice",
-            "Effect": "Allow",
-            "Action": [
-                "iam:CreateVirtualMFADevice",
-                "iam:EnableMFADevice",
-                "iam:ResyncMFADevice",
-                "iam:DeleteVirtualMFADevice"
-            ],
-            "Resource": [
-                "arn:aws:iam::${data.aws_caller_identity.current.account_id}:mfa/$${aws:username}",
-                "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/$${aws:username}"
-            ]
-        },
-        {
-            "Sid": "AllowUsersToDeactivateTheirOwnVirtualMFADevice",
-            "Effect": "Allow",
-            "Action": [
-                "iam:DeactivateMFADevice"
-            ],
-            "Resource": [
-                "arn:aws:iam::${data.aws_caller_identity.current.account_id}:mfa/$${aws:username}",
-                "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/$${aws:username}"
-            ],
-            "Condition": {
-                "Bool": {
-                    "aws:MultiFactorAuthPresent": true
-                }
-            }
-        },
-        {
-            "Sid": "AllowUsersToListMFADevicesandUsersForConsole",
-            "Effect": "Allow",
-            "Action": [
-                "iam:ListMFADevices",
-                "iam:ListVirtualMFADevices"
-            ],
-            "Resource": "*"
-        }
-    ]
-}
-EOF
+    policy = "${data.aws_iam_policy_document.self_manage_iam_user.json}"
 }
