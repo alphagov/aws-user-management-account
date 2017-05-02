3453286dcd6f82adc91e0a8e07618c31b8f3e239
David King <david.king@digital.cabinet-office.gov.uk>
2017-05-02 17:42:12 +0100

    Include CloudTrail configuration and CloudWatch alarms
    These will trigger notifications when
     - Root account has activity
     - Unauthorized API calls are made

---
+++ terraform/cloudwatch-alarms.tf
@@ -0,0 +1,31 @@
+variable "environment_name" {
+  type = "string"
+}
+
+variable "cloudtrail_s3_bucket_name" {
+  type = "string"
+}
+
+variable "cloudtrail_s3_bucket_prefix" {
+  type = "string"
+}
+
+module "aws_security_alarms" {
+  source                      = "github.com/alphagov/aws-security-alarms/terraform"
+  cloudtrail_s3_bucket_name   = "${var.cloudtrail_s3_bucket_name}"
+  cloudtrail_s3_bucket_prefix = "${var.cloudtrail_s3_bucket_prefix}"
+}
+
+module "root_activity" {
+  source               = "github.com/alphagov/aws-security-alarms/terraform/alarms/root_activity"
+  cloudtrail_log_group = "${module.aws_security_alarms.cloudtrail_log_group}"
+  alarm_actions        = ["${module.aws_security_alarms.security_alerts_topic}"]
+  environment_name     = "${var.environment_name}"
+}
+
+module "unauthorized_activity" {
+  source               = "github.com/alphagov/aws-security-alarms/terraform/alarms/unauthorized_activity"
+  cloudtrail_log_group = "${module.aws_security_alarms.cloudtrail_log_group}"
+  alarm_actions        = ["${module.aws_security_alarms.security_alerts_topic}"]
+  environment_name     = "${var.environment_name}"
+}
