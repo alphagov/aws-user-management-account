d7036adbd4ef1b42d570de55d6342e0625f10370
David King <david.king@digital.cabinet-office.gov.uk>
2018-03-09 15:32:50 +0000

    Inline the 2 variables currently configured
    This makes it easier for others to contribute to the Terraform code.
    
    Previously the variables were kept separates so we didn't publish the name of
    the S3 bucket storing CloudTrail logs. That bucket has been around for a while
    now, no need to keep the bucket name private.

--- terraform/cloudwatch-alarms.tf
+++ terraform/cloudwatch-alarms.tf
@@ -1,14 +1,6 @@
-variable "environment_name" {
-  type = "string"
-}
-
-variable "cloudtrail_s3_bucket_name" {
-  type = "string"
-}
-
 module "aws_security_alarms" {
   source                      = "github.com/alphagov/aws-security-alarms/terraform"
-  cloudtrail_s3_bucket_name   = "${var.cloudtrail_s3_bucket_name}"
+  cloudtrail_s3_bucket_name   = "gds-audit-cloudtrails"
   cloudtrail_s3_bucket_prefix = "${data.aws_caller_identity.current.account_id}"
 }
 
@@ -16,5 +8,5 @@ module "root_activity" {
   source               = "github.com/alphagov/aws-security-alarms/terraform/alarms/root_activity"
   cloudtrail_log_group = "${module.aws_security_alarms.cloudtrail_log_group}"
   alarm_actions        = ["${module.aws_security_alarms.security_alerts_topic}"]
-  environment_name     = "${var.environment_name}"
+  environment_name     = "gds-user-management"
 }
