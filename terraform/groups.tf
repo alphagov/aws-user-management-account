744069001e414be5ffd2486de07b0a90f7618bf6
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2018-09-28 15:55:18 +0100

    remove the Administrators group
    This group had already been removed manually, so this change just
    updates the terraform to reflect reality.
    
    Also, we've decided that people should only perform admin privileges
    when they have actively decided to assume and administrative role, and
    not have those powers ambiently via group membership.

--- terraform/groups.tf
+++ terraform/groups.tf
@@ -11,12 +11,3 @@ resource "aws_iam_group_policy_attachment" "cross-account-attach-assume-role" {
   group      = "${aws_iam_group.cross-account-access.name}"
   policy_arn = "${aws_iam_policy.assume_any_role.arn}"
 }
-
-resource "aws_iam_group" "administrators" {
-  name = "Administrators"
-}
-
-resource "aws_iam_group_policy_attachment" "administrators-policy" {
-  group      = "${aws_iam_group.administrators.name}"
-  policy_arn = "arn:aws:iam::aws:policy/IAMFullAccess"
-}
