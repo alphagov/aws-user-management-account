0266fa540e02932d2f4793ecba93024533f43a2f
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2021-10-26 09:58:33 +0100

    Fix terraform
    We didn't pin the version of terraform and so stuff is broken.
    
    This needs upgrading to 1.0 at some point.

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,7 +1,13 @@
 terraform {
+  required_version = "0.12.20"
+
   backend "s3" {
     bucket = "gds-user-management-terraform"
     key    = "terraform/state/user-management-account"
     region = "eu-west-2"
   }
+}
+
+provider "aws" {
+  version = "~> 2.0"
 }
